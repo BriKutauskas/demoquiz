6bdc6f4b2c4671a5d0628c317f8a34011bb54bd6
Brianna Kutauskas <briannakutauskas@Briannas-MacBook-Pro.local>
2017-04-24 19:14:00 -0700

    back button

--- main.js
+++ main.js
@@ -2,33 +2,49 @@ var allQuestion = [
   {
     question: "Who is the cutest dog?",
     choices:['Gussel', 'Shirles', 'Spot', 'Roscoe'],
-    correctAnswer:0
+    correctAnswer: 0,
+    selectedAnswer: null
   },
   {
     question:'How old is Gussel?',
     choices:['3', '4', '5', '6'],
-    correctAnswer: 2
+    correctAnswer: 2,
+    selectedAnswer: null
   },
   {
     question:'True or false: Gussel is wearing a blue banadana',
     choices:['true', 'false'],
-    correctAnswer: 0
+    correctAnswer: 0,
+    selectedAnswer: null
   },
   {
     question: "Where is Gussel's favorite place to go?",
     choices:['park', 'beach', 'mountains', 'all of the above'],
-    correctAnswer: 3
+    correctAnswer: 3,
+    selectedAnswer: null
   },
   {
     question: 'True or false: Gussel hates playing with balls.',
     choices: ['true', 'false'],
-    correctAnswer: 1
+    correctAnswer: 1,
+    selectedAnswer: null
   }
 ];
 var position = 0;
 var firstQuestion = allQuestion[position].question;
+var radioSelect = function() {
+  var total = document.querySelector('input[name="answer"]:checked');
+  allQuestion[position].selectedAnswer = total.id;
+  console.log('poop');
+};
 var question = function()
 {
+  if (!position){
+    document.getElementById("back").style.visibility = "hidden";
+  }
+  else{
+    document.getElementById("back").style.visibility = "visible";
+  }
   firstQuestion = document.getElementById('questions');
   firstQuestion.innerHTML = allQuestion[position].question;
   var buttons = allQuestion[position].choices;
@@ -40,6 +56,8 @@ var question = function()
     var breaks = document.createElement('br');
     button.setAttribute('type','radio');
     button.setAttribute('name', 'answer');
+    button.setAttribute('id', 'answers' + i);
+    button.onclick = radioSelect;
     if(i === allQuestion[position].correctAnswer)
     {
       button.value = 1;
@@ -69,7 +87,18 @@ var onClick = function()
     else {
       position++;
       question();
+      if(allQuestion[position].selectedAnswer !== null){
+        document.getElementById(allQuestion[position].selectedAnswer).checked = true;
+      }
+      $("#questions").fadeIn();
       totalScore = totalScore + (+total.value);
     }
   }
+  total= allQuestion[position].selectedAnswer;
+};
+
+var back = function() {
+  position--;
+  question();
+  document.getElementById(allQuestion[position].selectedAnswer).checked = true;
 };
